7af294f71295c7195649e76eeef07d29b6b5ed30
Ivy Gross <ivy.gross4@gmail.com>
2023-12-28 17:50:36 -0500

    EXERCISE 4 ADDED

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+// const removeFromArray = function (array, ...removals) {
+//   let results = [];
+//   let excluded = [...removals];
+//   array.forEach((element) => {
+//     if (!excluded.includes(element)) {
+//       results.push(element);
+//     }
+//   });
+//   return results;
+// };
 
+const removeFromArray = (array, ...removals) => {
+  return array.filter((element) => !removals.includes(element));
 };
 
 // Do not edit below this line
