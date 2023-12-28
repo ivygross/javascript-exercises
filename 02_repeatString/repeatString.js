f06af3dc10035f3a1b24f3d03a724ba477fdec24
Ivy Gross <ivy.gross4@gmail.com>
2023-12-28 16:13:16 -0500

    SOLVED EXERCISES 1-3 WITH JEST TESTS

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function (word, times) {
+  if (times < 0) return "ERROR";
+  // if (times === 0) return "";
 
+  // let result = [];
+  let result = "";
+  let i = 0;
+  while (i < times) {
+    // result.push(word);
+    result += word;
+    i++;
+  }
+  // return result.join('');
+  return result;
 };
 
 // Do not edit below this line
